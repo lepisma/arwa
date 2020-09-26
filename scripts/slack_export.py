eef96890591d3c1fb83323afb9aaf1e0b985375c
Abhinav Tushar <abhinav@lepisma.xyz>
2020-09-26 12:10:02 +0530

    simplify exporter

--- scripts/slack_export.py
+++ scripts/slack_export.py
@@ -1,9 +1,15 @@
 """
-Export messages from slack.
+Export messages from slack to a jsonl file.
+
+Needs SLACK_USER_TOKEN with *:history scopes set. Mostly I am using
+channels:history and im:history for now.
 
 Usage:
-  slack_export.py channel <channel-id> --output-jsonl=<output-jsonl>
-  slack_export.py dm <user-id> --output-jsonl=<output-jsonl>
+  slack_export.py <c-id> --output-jsonl=<output-jsonl>
+
+Arguments:
+  <c-id>       Conversations id. This can be channel id, dm id or anything
+               that behaves like a conversation in slack terminology.
 """
 
 import os
@@ -15,12 +21,12 @@ import jsonlines
 import slack
 
 
-def get_message_batches(client, channel_id: str):
+def get_message_batches(client, c_id: str):
     """
     Get messages in batches of requests.
     """
 
-    response = client.conversations_history(channel=channel_id)
+    response = client.conversations_history(channel=c_id)
     message_batch = response["messages"]
 
     if not message_batch:
@@ -30,7 +36,7 @@ def get_message_batches(client, channel_id: str):
 
     while response["has_more"]:
         next_cursor = response["response_metadata"]["next_cursor"]
-        response = client.conversations_history(channel=channel_id, cursor=next_cursor)
+        response = client.conversations_history(channel=c_id, cursor=next_cursor)
         yield response["messages"]
 
 
@@ -39,15 +45,8 @@ if __name__ == "__main__":
 
     client = slack.WebClient(os.environ["SLACK_USER_TOKEN"])
 
-    if args["channel"]:
-        channel_id = args["<channel-id>"]
-    elif args["user-id"]:
-        channel_id = args["<user-id>"]
-    else:
-        raise NotImplementedError()
-
     with jsonlines.open(args["--output-jsonl"], mode="w") as fp:
         bar = tqdm()
-        for batch in get_message_batches(client, channel_id):
+        for batch in get_message_batches(client, args["<c-id>"]):
             fp.write_all(batch)
             bar.update(len(batch))
