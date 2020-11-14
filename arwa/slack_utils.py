36a3f7e89dcd9091e992796f56f72563bd355370
Abhinav Tushar <abhinav@lepisma.xyz>
2020-11-14 20:44:39 +0530

    last user access

--- arwa/slack_utils.py
+++ arwa/slack_utils.py
@@ -1,9 +1,33 @@
 from typing import Dict, Iterator, List
 
-from pydash import py_
-
 import slack
+from pydash import py_
+from tqdm import trange
+
 from arwa.types import SlackUser
+
+
+def last_user_access(client: slack.WebClient, n_pages=10) -> Dict[SlackUser, Dict]:
+    """
+    Return latest user slack access information.
+
+    HACK: This is an approximation and since we are only going to use this for
+          geolocation, it's alright.
+    """
+
+    access = {}
+
+    for page_number in trange(1, n_pages + 1):
+        response = client.team_accessLogs(count=100, page=page_number)
+
+        # We probably don't need sorting
+        for login in sorted(response["logins"], key=lambda it: it["date_last"], reverse=True):
+            # Note that this name does not align with names from other sources
+            user = SlackUser(login["user_id"], login["username"])
+            if user not in access:
+                access[user] = login
+
+    return access
 
 
 def list_users(client: slack.WebClient) -> List[SlackUser]:
