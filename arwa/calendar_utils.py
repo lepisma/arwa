1888b93a779f72f207e6c22f79a5ae6c223ef6be
Abhinav Tushar <abhinav@lepisma.xyz>
2021-02-14 00:35:25 +0530

    remove icalendar support

--- arwa/calendar_utils.py
+++ arwa/calendar_utils.py
@@ -5,7 +5,6 @@ Utilities for interacting with online calendars.
 import datetime
 from typing import List
 
-import icalendar
 from gcsa.google_calendar import GoogleCalendar
 from pydash import py_
 
@@ -26,14 +25,6 @@ def get_last_sunday(dt=None) -> datetime.datetime:
 
     delta = datetime.timedelta(days=day % 7)
     return _date_to_dt(dt - delta)
-
-
-def parse_icalendar_event(ev) -> CalendarEvent:
-    return CalendarEvent(
-        name=str(ev["SUMMARY"]),
-        start_time=ev["DTSTART"].dt,
-        end_time=ev["DTEND"].dt
-    )
 
 
 def parse_google_calendar(email_id: str, start_time: datetime.datetime, end_time: datetime.datetime) -> List[CalendarEvent]:
@@ -68,23 +59,6 @@ def parse_google_calendar(email_id: str, start_time: datetime.datetime, end_time
             end_time=end_time,
             response_status=response_status
         ))
-
-    return events
-
-
-def parse_ics(ics_file: str) -> List[CalendarEvent]:
-    """
-    Parse calendar events from an ics file. Only events with start and end
-    times are parsed.
-    """
-
-    with open(ics_file) as fp:
-        cal = icalendar.Calendar.from_ical(fp.read())
-
-    events = []
-    for it in cal.subcomponents:
-        if isinstance(it, icalendar.Event) and "DTEND" in it:
-            events.append(parse_icalendar_event(it))
 
     return events
 
